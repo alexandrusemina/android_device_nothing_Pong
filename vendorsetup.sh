30908991c765ea64b95faef85eb7af8b0c2e2d28
dhruvesh16 <dhruvesh3466@protonmail.com>
2024-12-20 22:41:47 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,14 +3,16 @@ rm -rf hardware/qcom-caf/sm8450/display
 git clone https://github.com/Nothing-phone-2-Development/android_hardware_qcom_display.git hardware/qcom-caf/sm8450/display 
 
 cd hardware/qcom-caf/sm8450/audio/primary-hal
-git fetch https://github.com/Nothing-phone-2-Development/android_hardware_qcom_audio-ar.git 
+git fetch https://github.com/Nothing-phone-2-Development/android_hardware_qcom_audio-ar.git lineage-21.0-caf-sm8450
 git cherry-pick d2c5aca74a58d80970902972dc710acbbdaf4c56
+git fetch https://github.com/Nothing-phone-2-Development/android_hardware_qcom_audio-ar.git lineage-22.0-caf-sm8450
+git cherry-pick d41ba854a239b9d00e85a4b3367ace65dd2f4c47
 cd ../../../../..
 
 cd device/qcom/sepolicy_vndr/sm8450 
 git fetch https://github.com/Nothing-phone-2-Development/android_device_qcom_sepolicy_vndr.git
-git cherry-pick 7cd972ffc847631f0a892e4d13eb1def59982268 066c4d78b75d9af23bf3ff7a67c73a64c9acc810 fb4279c23fdd110ae0e13bdb72f36515242b866d
+git cherry-pick e63f86c2e43b021f34a1258e068964e72a91c395
 cd ../../../..
 
-git clone https://github.com/Nothing-phone-2-Development/android_kernel_nothing_sm8475.git kernel/nothing/sm8475
+git clone --depth=1 https://github.com/HELLBOY017/kernel_nothing_sm8475.git -b inline-2 kernel/nothing/sm8475 && cd kernel/nothing/sm8475 && git submodule init && git submodule update --remote && cd ../../..
 git lfs clone https://gitea.com/dhruvesh17/vendor_nothing_Pong.git vendor/nothing/Pong
